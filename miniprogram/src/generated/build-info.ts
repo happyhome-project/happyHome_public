26a7f51267b561d21ad8f05ba00334c1c7b23b34
AngryBird <48046333+angrybirddd@users.noreply.github.com>
2026-06-03 05:06:57 +0800

    chore: mark trial build 1.0.2606030504

--- miniprogram/src/generated/build-info.ts
+++ miniprogram/src/generated/build-info.ts
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
-  version: "1.0.2606022321",
-  desc: "trial 2026-06-02 23:21 cbd1c0b",
-  buildId: "mp-1.0.2606022321",
+  version: "1.0.2606030504",
+  desc: "trial 2026-06-03 05:04 2f2bb71",
+  buildId: "mp-1.0.2606030504",
 }
