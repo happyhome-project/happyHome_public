1b5daa3781301c7247f9a59837a2a86b9adf9f92
AngryBird <48046333+angrybirddd@users.noreply.github.com>
2026-06-09 11:52:40 +0800

    chore: record 2026-06-09 release build

--- miniprogram/src/generated/build-info.ts
+++ miniprogram/src/generated/build-info.ts
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
-  version: "1.0.2606032111",
-  desc: "trial 2026-06-03 21:11 5647098",
-  buildId: "mp-1.0.2606032111",
+  version: "1.0.2606091140",
+  desc: "trial 2026-06-09 11:40 3898304",
+  buildId: "mp-1.0.2606091140",
 }
