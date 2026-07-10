483942a57b8f816d8e6ed3aca844f3c684323d6a
AngryBird <48046333+angrybirddd@users.noreply.github.com>
2026-07-10 19:17:01 +0800

    chore: mark release 1.0.2607101906

--- miniprogram/src/generated/build-info.ts
+++ miniprogram/src/generated/build-info.ts
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
-  version: "1.0.2607091735",
-  desc: "trial 2026-07-09 17:35 f3c94d4",
-  buildId: "mp-1.0.2607091735",
+  version: "1.0.2607101906",
+  desc: "trial 2026-07-10 19:06 fef403a",
+  buildId: "mp-1.0.2607101906",
 }
